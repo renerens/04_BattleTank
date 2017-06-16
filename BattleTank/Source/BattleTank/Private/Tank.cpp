c39bf739e45e8c43ef26df1f2c83f557aed331b0
RENEGADE-PC\Renegade <rene.rens@gmail.com>
2017-06-16 21:26:50 +0200

    Create DefaultSubobject in C++

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -8,7 +8,8 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// no need to protect points as added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -34,6 +35,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimtAt(FVector HitLocation)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s Amiming at %s"),*GetName(), *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Public/TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -17,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 
 private:	
 	// Sets default values for this pawn's properties
