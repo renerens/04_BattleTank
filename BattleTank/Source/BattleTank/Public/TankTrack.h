289ce3509db55858ae170eb2cbf9e8ab956b12c2
RENEGADE-PC\Renegade <rene.rens@gmail.com>
2017-06-17 23:57:08 +0200

    ApplyForceAtLocation() in Action

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -19,5 +19,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank and 1 g acceleration
+
 private:
 };
